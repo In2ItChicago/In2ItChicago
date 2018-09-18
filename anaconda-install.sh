4ccadd11a6e18c7316d3e2f0e9992808bba92258
Austin Schey <aschey13@gmail.com>
2018-09-17 22:19:09 -0500

    updated db client to return fields for django site and use flask_pymongo library

--- anaconda-install.sh
+++ anaconda-install.sh
@@ -1,4 +1,4 @@
 conda remove beautifulsoup4 scrapy pymongo flask
-pip uninstall daterangeparser icalendar beaker
+pip uninstall daterangeparser icalendar beaker flask_pymongo
 conda install -c conda-forge beautifulsoup4 scrapy pymongo flask
-pip install daterangeparser icalendar beaker
+pip install daterangeparser icalendar beaker flask_pymongo
