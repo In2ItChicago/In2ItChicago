3427c769643177bb6692cf4416d7d41d3f90d524
Austin Schey <aschey13@gmail.com>
2018-09-25 00:45:08 -0500

    fixed python 3.7 support and other small changes

--- clipboard_common_lib/clipboardcommonlib/shared_config.py
+++ clipboard_common_lib/clipboardcommonlib/shared_config.py
@@ -4,8 +4,10 @@ import time
 import requests
 
 class Config:
-    def __init__(self):
-        self.db_client_ip = self.get_env_var('DB_CLIENT_IP')
+    def __init__(self, local_db_client=False):
+        self.local_db_client = local_db_client
+
+        self.db_client_ip = self.get_client_ip()
         self.db_client_port = 5000
         self.db_client_url = f'http://{self.db_client_ip}:{self.db_client_port}'
 
@@ -29,6 +31,16 @@ class Config:
             return os.environ[name]
         except KeyError:
             raise KeyError(f'Error: environment variable {name} not set. If this value was recently set, close all python processes and try again')
+
+    def get_client_ip(self):
+        if self.local_db_client:
+                return 'localhost'
+        elif self.get_env_var('DB_CLIENT_IP') == 'clipboard_db_client':
+            # data engine is running in Docker
+            return 'clipboard_db_client'
+        else:
+            # db client is running in Docker but data engine is not
+            return self.get_env_var('DOCKER_IP')
     
     def connect_to_client(self):
         for _ in range(self.num_connect_attempts):
--- install-common-libs.sh
+++ install-common-libs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 if command -v python3 &>/dev/null; then
 	pythoncmd=python3
-	pipcmd=pip3
 else
 	pythoncmd=python
-	pipcmd=pip
 fi
 
 cd clipboard_common_lib
 $pythoncmd setup.py sdist
 cd ..
-$pipcmd install ./clipboard_common_lib
+$pythoncmd -m pip install ./clipboard_common_lib
