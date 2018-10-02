127235fcfb50670b5510a5cc3d08625a8e697a4d
Austin Schey <aschey13@gmail.com>
2018-10-01 22:42:36 -0500

    config changes

--- anaconda-install.sh
+++ anaconda-install.sh
@@ -1,6 +1,5 @@
 conda remove beautifulsoup4 scrapy pymongo flask django twisted
 pip uninstall daterangeparser icalendar beaker flask_pymongo twisted
-#pip install git+https://github.com/twisted/twisted@trunk --upgrade
-conda install -c conda-forge beautifulsoup4 scrapy pymongo flask django twisted
-pip install daterangeparser icalendar beaker flask_pymongo
+conda install -c conda-forge twisted beautifulsoup4 scrapy pymongo flask django
+pip install daterangeparser icalendar beaker flask_pymongo djongo
 read -n 1 -s -r -p "Press any key to continue"
--- build.sh
+++
@@ -1,3 +0,0 @@
-./scripts/install-common-libs.sh
-docker-compose build
-./scripts/check_all_image_updates.sh
