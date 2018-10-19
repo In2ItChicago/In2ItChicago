9eca690111eba6fb1320a2540ec3b137bfc4ff3c
Austin Schey <aschey13@gmail.com>
2018-10-18 23:18:33 -0500

    removed f strings because aws instance is too old, ignore generated files, add python version selector

--- render.py
+++ render.py
@@ -5,10 +5,9 @@ import sys
 files = sys.argv[1:]
 for file in files:
     rendered = None
-    #with open('nginxTest.conf.j2', 'r') as f:
     new_file = file.split('.j2')[0]
     if new_file == file:
-        print(f'File {file} must end in .j2')
+        print('File ' + file + ' must end in .j2')
         continue
 
     with open(file, 'r') as f:
--- render.sh
+++ render.sh
@@ -1,1 +1,8 @@
-python render.py nginx/nginx.conf.j2
+#!/bin/bash
+if command -v python3 &>/dev/null; then
+	pythoncmd=python3
+else
+	pythoncmd=python
+fi
+
+pythoncmd render.py nginx/nginx.conf.j2
