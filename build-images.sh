42ae7be3c9048e47ce5090da798a6979353395ba
Austin <aschey13@gmail.com>
2018-12-02 23:24:37 -0600

    modified dev and prod config

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 docker build -t data_engine data_engine
-docker build -t clipboard_site clipboard_site
+docker build -t clipboard_site clipboard_site --target=clipboard_site_$1
 docker build -t clipboard_db_client clipboard_db_client
