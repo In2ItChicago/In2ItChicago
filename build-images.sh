762621c8566cd982f5f1e346596d55d228801d2c
Austin <aschey13@gmail.com>
2018-12-04 00:58:56 -0600

    starting kubernetes config

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,9 @@
-docker build -t data_engine data_engine
-docker build -t clipboard_site clipboard_site --target=clipboard_site_$1
-docker build -t clipboard_db_client clipboard_db_client
+docker build -t data_engine data_engine --progress=plain
+docker build -t clipboard_site clipboard_site --target=clipboard_site_$1 --progress=plain
+docker build -t clipboard_db_client clipboard_db_client --progress=plain
+docker tag data_engine aschey/data_engine
+docker tag clipboard_site aschey/clipboard_site
+docker tag clipboard_db_client aschey/clipboard_db_client
+docker push aschey/data_engine
+docker push aschey/clipboard_site
+docker push aschey/clipboard_db_client
