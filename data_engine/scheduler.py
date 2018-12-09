53e7841027c127fd0a118979ffb12c29bc2b2163
Austin Schey <aschey13@gmail.com>
2018-12-09 00:23:12 -0600

    changed scheduler to run periodically

---
+++ data_engine/scheduler.py
@@ -0,0 +1,54 @@
+from apscheduler.schedulers.twisted import TwistedScheduler
+from apscheduler.events import EVENT_JOB_MISSED
+from twisted.internet import reactor
+from dateutil.relativedelta import relativedelta
+from data_aggregators.apis.library_events import LibraryEvents
+from data_aggregators.apis.greatlakes_ical import GreatLakesReader
+from data_aggregators.clipboard_scrapers.spiders.history_spider import HistorySpider
+from data_aggregators.clipboard_scrapers.spiders.wpbcc_spider import WpbccSpider
+from data_aggregators.clipboard_scrapers.spiders.lwvchicago_spider import LWVchicago
+from threading import Lock
+from datetime import datetime
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+
+class Scheduler:
+    def __init__(self):
+        self.scrapers = [HistorySpider, WpbccSpider, LWVchicago, LibraryEvents, GreatLakesReader]
+        self.start_date = datetime.now().strftime('%m-%d-%Y')
+        self.end_date = (datetime.now() + relativedelta(months=+1)).strftime('%m-%d-%Y')
+        self.interval_seconds = 20
+
+        self.scheduler = TwistedScheduler()
+        self.scheduler.add_listener(self.schedule_missed, EVENT_JOB_MISSED)
+    
+    def add_schedule(self, scraper, seconds_delay):
+        self.scheduler.add_job(self.run_scraper, 
+                id=scraper.__name__, 
+                trigger='interval', 
+                args=[scraper], 
+                start_date=datetime.now() + relativedelta(seconds=seconds_delay), 
+                seconds=self.interval_seconds)
+        
+    def schedule_missed(self, event):
+        print(f'{event.job_id} missed. Interval time: {self.interval_seconds}')
+
+    def run_scraper(self, scraper):
+        print('starting ' + scraper.__name__)
+        runner = CrawlerRunner(get_project_settings())
+        runner.crawl(scraper, self.start_date, self.end_date)
+        runner.join()
+        #crawl_result.addBoth(lambda _: self.add_run(scraper))
+
+    
+    def run_schedule(self):
+        configure_logging()
+        start_interval = self.interval_seconds / len(self.scrapers)
+        now = datetime.now()
+        self.last_scheduled = now
+        for index, scraper in enumerate(self.scrapers):
+            self.add_schedule(scraper, start_interval * index)
+        
+        self.scheduler.start()
+        reactor.run()
---
+++ start-compose.bash
@@ -0,0 +1,14 @@
+# This file ends in .bash instead of .sh because the following command will modify this script
+# if it ends in .sh and modifying a script that's currently running causes a tear in the fabric of spacetime
+
+# Remove any windows-specific characters if running in a Windows environment
+case "$(uname)" in
+   CYGWIN*|MINGW*|MSYS*)
+     ./dos2unix.exe *.sh */*.sh
+     ;;
+esac
+ENV=${1:-dev}
+./render.sh
+./build-images.sh $ENV
+./check-all-image-updates.sh
+./compose-deploy.sh $ENV
