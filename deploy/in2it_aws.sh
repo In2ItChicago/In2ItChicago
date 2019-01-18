66f8aee5fa88662186243e9fec1ecbfdc9c165b1
Austin Schey <aschey13@gmail.com>
2019-01-17 20:41:04 -0600

    updated deploy scripts. Build cache can cause issues when deploying to prod

--- deploy/in2it_aws.sh
+++ deploy/in2it_aws.sh
@@ -1,1 +1,1 @@
-ssh -v -i ./in2it-key.pem ubuntu@3.17.64.55
+ssh -v -i $(dirname $0)/in2it-key.pem ubuntu@3.17.64.55
--- deploy/in2it_aws_deploy.sh
+++ deploy/in2it_aws_deploy.sh
@@ -1,5 +1,7 @@
-scp -v -i ./in2it-key.pem ./deploy.sh ubuntu@3.17.64.55:/home/ubuntu
-ssh -v -i ./in2it-key.pem ubuntu@3.17.64.55 << EOF
+docker system prune -af
+$(dirname $0)/publish-images.sh
+scp -v -i $(dirname $0)/in2it-key.pem $(dirname $0)/deploy.sh ubuntu@3.17.64.55:/home/ubuntu
+ssh -v -i $(dirname $0)/in2it-key.pem ubuntu@3.17.64.55 << EOF
 chmod +x ~/deploy.sh
 ~/deploy.sh
 EOF
