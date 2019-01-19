0cd7cf5c90f6baa91af0c23fb22172158cc5d6c1
Austin Schey <aschey13@gmail.com>
2019-01-19 00:17:21 -0600

    added port 80 to connections

--- deploy/connect_ports.sh
+++ deploy/connect_ports.sh
@@ -2,3 +2,4 @@ ssh -nNT -i $(dirname $0)/in2it-key.pem -L 3000:localhost:3000 ubuntu@3.17.64.55
 ssh -nNT -i $(dirname $0)/in2it-key.pem -L 5000:localhost:5000 ubuntu@3.17.64.55 &
 ssh -nNT -i $(dirname $0)/in2it-key.pem -L 9000:localhost:9000 ubuntu@3.17.64.55 &
 ssh -nNT -i $(dirname $0)/in2it-key.pem -L 27017:localhost:27017 ubuntu@3.17.64.55 &
+ssh -nNT -i $(dirname $0)/in2it-key.pem -L 80:localhost:80 ubuntu@3.17.64.55 &
