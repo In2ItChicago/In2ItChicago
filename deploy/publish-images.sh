916b613cfe12f7fd47955700ebb34b6d4bee6818
Austin Schey <aschey13@gmail.com>
2019-01-23 01:49:04 -0600

    updated images

--- deploy/publish-images.sh
+++ deploy/publish-images.sh
@@ -1,7 +1,7 @@
 $(dirname $0)/../scripts/build-images.sh prod --no-cache
-docker tag event_processor in2itchicago/event_processor
-docker tag in2it_site in2itchicago/in2it_site
-docker tag event_service in2itchicago/event_service
+docker tag event_processor in2itchicago/event_processor:$1
+docker tag in2it_site in2itchicago/in2it_site:$1
+docker tag event_service in2itchicago/event_service:$1
 docker push in2itchicago/event_processor
 docker push in2itchicago/in2it_site
 docker push in2itchicago/event_service
