629eadaf88e7170ab9009ea5e8da8401bd30c7c5
Austin Schey <aschey13@gmail.com>
2019-01-26 17:28:50 -0600

    command updates for ndscheduler, removed old files

---
+++ event_processor/setup.py
@@ -0,0 +1,10 @@
+# Automatically created by: scrapyd-deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'In2ItChicago',
+    version      = '1.0',
+    packages     = find_packages(),
+    entry_points = {'scrapy': ['settings = settings']},
+)
--- local-registry.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -e REGISTRY_HTTP_ADDR=0.0.0.0:5001 -p 5001:5001 --name registry registry:2
