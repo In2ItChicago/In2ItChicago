8a9861d8ec8240ae33e2e0937893c9ea859cf599
WesThorburn <wjthorburn@live.com.au>
2019-03-11 13:19:17 -0500

    Populate join-us and about-us pages

--- in2it_site/pages/join-us.vue
+++ in2it_site/pages/join-us.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        <h1>Join Us</h1>
+    <div class="content-col">
+        <h1>How to Help</h1>
+
+        <p>In2it is a team effort. We are thankful to the many people who have helped develop the project along the way.</p>
+
+        <p>If you are interested in helping to advance the project or are interested in helping us test, let us know!</p>
+
+
+        <h3>Contact us</h3>
+        <a href="mailto:theclipboardproject@gmail.com">theclipboardproject@gmail.com</a>
     </div>
 </template>
