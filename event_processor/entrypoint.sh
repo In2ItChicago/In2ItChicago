ecdd541854d26a6896057bb6c203bc8586c72c83
Austin Schey <aschey13@gmail.com>
2019-04-07 12:28:19 -0500

    Switch containers to alpine

--- event_processor/entrypoint.sh
+++ event_processor/entrypoint.sh
@@ -6,6 +6,7 @@ then
     rm -rf logs
     rm -rf dbs
     chmod +x deploy.sh
+    until ping -c1 ndscheduler >/dev/null 2>&1; do :; done
     # -u is passed to python to return output unbuffered, so there is no delay when viewing docker logs
     python -u create_schedules.py
     sleep 5
