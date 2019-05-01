41cc8c2c93063c09aa4a319256991ec7c8f40fbd
Austin Schey <aschey13@gmail.com>
2019-05-01 00:40:54 -0500

    try/catch cache calls, fix bug in start.sh

--- event_processor/cache_call.py
+++ event_processor/cache_call.py
@@ -1,4 +1,5 @@
 import functools
+import logging
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 from switchable_decorator import SwitchableDecorator
@@ -10,4 +11,13 @@ cache = CacheManager(**parse_cache_config_options({
         'cache.lock_dir': '/tmp/beaker/lock'
     }))
 
-cache_call = SwitchableDecorator(cache.cache('web_call', expire=config.api_cache_expiration), config.enable_api_cache)
+def try_cache(target):
+    def try_call(*args, **kwargs):
+        try:
+            return cache.cache('web_call', expire=config.api_cache_expiration)(target)(*args, **kwargs)
+        except Exception as e:
+            logging.getLogger('scrapy').warning('Exception while calling cache: ' + str(e))
+        return target(*args, **kwargs)
+    return try_call
+    
+cache_call = SwitchableDecorator(try_cache, config.enable_api_cache)
--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
-ENV=${1:-dev}
 ./scripts/create-network.sh
 if [ "$CHECK_IMAGE_UPDATES" == "1" ]
 then
     ./scripts/check-all-image-updates.sh
 fi
 
-./scripts/build-images.sh $ENV
+./scripts/build-images.sh dev
 docker-compose down
 docker-compose rm -f -v
 ./db/run.sh &
