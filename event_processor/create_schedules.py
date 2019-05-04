d54f1cee887682ca51f7cd1b1a031b31a7dbd0d8
Austin Schey <aschey13@gmail.com>
2019-05-04 00:48:27 -0500

    Don't break after updating first schedule, allow debugger to attach during schedule creation

--- event_processor/create_schedules.py
+++ event_processor/create_schedules.py
@@ -5,9 +5,12 @@ from scrapy import spiderloader
 from scrapy.utils import project
 from config import config
 from requests.exceptions import ConnectionError
+import ptvsd
+
 HOURS_IN_DAY = 24
 MINUTES_IN_HOUR = 60
 JOB_CLASS = 'simple_scheduler.jobs.scrapy_job.ScrapyJob'
+ATTEMPTS = 100
 
 class Schedule:
     def __init__(self, name, interval, delta, offset):
@@ -20,44 +23,50 @@ class Schedule:
             self.minute = f'{current_offset}-{MINUTES_IN_HOUR - 1}/{interval}'
             self.hour = '*'
 
-ATTEMPTS = 100
-settings = project.get_project_settings()
-spider_loader = spiderloader.SpiderLoader.from_settings(settings)
-spiders = spider_loader.list()
-classes = [s for s in (spider_loader.load(name) for name in spiders) if s.enabled]
+def run():
+    settings = project.get_project_settings()
+    spider_loader = spiderloader.SpiderLoader.from_settings(settings)
+    spiders = spider_loader.list()
+    classes = [s for s in (spider_loader.load(name) for name in spiders) if s.enabled]
 
-for _ in range(ATTEMPTS):
-    try:
-        scrapy_jobs = requests.get(config.scheduler_jobs)
-        break
-    except ConnectionError:
-        time.sleep(5)
-jobs_json = scrapy_jobs.json()
-jobs_dict = {job['name']: job for job in jobs_json['jobs'] if job['job_class_string'] == JOB_CLASS}
-
-delta = math.ceil(config.schedule_interval / len(classes))
-
-schedules = [Schedule(scraper.name, config.schedule_interval, delta, index) for index, scraper in enumerate(classes)]
-
-for schedule in schedules:
-    json_payload = {
-            'hour': schedule.hour, 
-            'minute': schedule.minute, 
-            'name': schedule.name,
-            'job_class_string': JOB_CLASS,
-            'pub_args': [schedule.name]
-        }
-    if schedule.name in jobs_dict:
-        job = jobs_dict[schedule.name]
-        response = requests.put(f'{config.scheduler_jobs}/{job["job_id"]}', json=json_payload)
-        if response.ok:
-            print(f'Updated schedule for {schedule.name}')
+    for _ in range(ATTEMPTS):
+        try:
+            scrapy_jobs = requests.get(config.scheduler_jobs)
             break
+        except ConnectionError:
+            time.sleep(5)
+    jobs_json = scrapy_jobs.json()
+    jobs_dict = {job['name']: job for job in jobs_json['jobs'] if job['job_class_string'] == JOB_CLASS}
+
+    delta = math.ceil(config.schedule_interval / len(classes))
+
+    schedules = [Schedule(scraper.name, config.schedule_interval, delta, index) for index, scraper in enumerate(classes)]
+
+    for schedule in schedules:
+        json_payload = {
+                'hour': schedule.hour, 
+                'minute': schedule.minute, 
+                'name': schedule.name,
+                'job_class_string': JOB_CLASS,
+                'pub_args': [schedule.name]
+            }
+        if schedule.name in jobs_dict:
+            job = jobs_dict[schedule.name]
+            response = requests.put(f'{config.scheduler_jobs}/{job["job_id"]}', json=json_payload)
+            if response.ok:
+                print(f'Updated schedule for {schedule.name}')
+            else:
+                raise Exception(response.text)
         else:
-            raise Exception(response.text)
-    else:
-        response = requests.post(config.scheduler_jobs, json=json_payload)
-        if response.ok:
-            print(f'Added schedule for {schedule.name}')
-        else:
-            raise Exception(response.text)
+            response = requests.post(config.scheduler_jobs, json=json_payload)
+            if response.ok:
+                print(f'Added schedule for {schedule.name}')
+            else:
+                raise Exception(response.text)
+
+if __name__ == '__main__':
+    if config.debug:
+        ptvsd.enable_attach(address=('0.0.0.0', 5860))
+        ptvsd.wait_for_attach()
+        
+    run()
