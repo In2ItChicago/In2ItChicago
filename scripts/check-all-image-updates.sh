81599d5d2d4bdefaa78af0ae5c815114a5ad77ca
Austin Schey <aschey13@gmail.com>
2019-05-04 16:22:15 -0500

    Updated image and package versions, replaced nodemon with ts-node-dev for event service

--- scripts/check-all-image-updates.sh
+++ scripts/check-all-image-updates.sh
@@ -12,10 +12,10 @@ echo "============================="
 echo "Checking for image updates..."
 $pythoncmd $scriptpath library/python 3.7.3 alpine
 $pythoncmd $scriptpath library/postgres 11.2 alpine
-$pythoncmd $scriptpath dpage/pgadmin4 4.5
-$pythoncmd $scriptpath library/nginx 1.15.11 alpine
-$pythoncmd $scriptpath library/nginx 1.15.11
-$pythoncmd $scriptpath library/node 11.14 alpine
+$pythoncmd $scriptpath dpage/pgadmin4 4.6
+$pythoncmd $scriptpath library/nginx 1.16 alpine
+$pythoncmd $scriptpath library/nginx 1.16
+$pythoncmd $scriptpath library/node 12.1 alpine
 $pythoncmd $scriptpath portainer/portainer 1.20.2
 echo "=============================="
 echo
