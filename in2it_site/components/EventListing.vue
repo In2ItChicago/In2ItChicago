c55fad8b24d18f46c75f15f4f71868fb3052d8da
WesThorburn <wjthorburn@live.com.au>
2019-06-13 02:15:35 +0800

    Add date, time and location to event listing cards

--- in2it_site/components/EventListing.vue
+++ in2it_site/components/EventListing.vue
@@ -2,7 +2,10 @@
     <div @click="navigateToEventPage()" class="event-listing d-flex flex-row event-listing-link">
         <div class="d-flex flex-column">
             <div class="d-flex w-100">
-                <h2 class="event-listing-heading mb-1">{{ event.title }}</h2>
+                <h2 class="event-listing-title mb-1">{{ event.title }}</h2>
+            </div>
+            <div class="d-flex w-100">
+                <h3 class="event-listing-time-location mb-1">{{ event.start_date }} {{ event.start_time  }}, {{ event.address }}</h3>
             </div>
             <p class="event-listing-description mb-1">{{ event.description }}</p>
         </div>
