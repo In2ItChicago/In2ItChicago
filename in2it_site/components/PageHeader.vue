e3d457c2c0eb1afd4d8b32e114a776113a655481
WesThorburn <wjthorburn@live.com.au>
2019-06-13 01:33:16 +0800

    Add In2It logo to page header

--- in2it_site/components/PageHeader.vue
+++ in2it_site/components/PageHeader.vue
@@ -2,8 +2,11 @@
 	<nav class="navbar navbar-expand-lg navbar-light in2it-navbar">
         <div class="container">
             <div class="d-flex flex-column">
-                <a class="in2it-brand" href="/">In2It</a>
                 <div class="d-flex flex-row">
+                    <a class="in2it-brand" href="/">
+                        <img class="navbar-logo" src="/img/in2it-logo.png">
+                    </a>
+
                     <a class="nav-link" href="/">Home</a>
                     <a class="nav-link" href="/about-us">About Us</a>
                     <a class="nav-link" href="/faq">FAQ</a>
