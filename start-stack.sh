ba1deec48a1bee468c396b3f4f490667b2859195
Austin Schey <aschey13@gmail.com>
2019-07-22 12:23:36 -0500

    Wait for db deploy when using docker stack

--- start-stack.sh
+++ start-stack.sh
@@ -6,4 +6,7 @@ sleep 5
 ./scripts/create-network.sh
 ./scripts/build-images.sh $ENV
 ./db/run.sh &
+DB_PID=$!
 ./scripts/stack-deploy.sh $ENV
+echo "Waiting for db deploy. PID: ${DB_PID}"
+wait $DB_PID
