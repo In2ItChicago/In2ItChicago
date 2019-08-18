8df8d741288a4146af2e0b21d923d923995e10c1
Austin Schey <aschey13@gmail.com>
2019-08-18 12:47:40 -0500

    Remove unused variable

--- in2it_site/plugins/notifications.client.js
+++ in2it_site/plugins/notifications.client.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import Notifications from 'vue-notification';
 
-const VueNotifications = {
-    install(Vue, options) {
-        Vue.component('notifications', Notifications);
-    }
-}
 Vue.use(Notifications);
 
 export default Notifications;
