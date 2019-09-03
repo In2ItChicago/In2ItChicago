f61ed62994e034171abd895939787b7fc153ba70
Wes <westhorburn@gmail.com>
2019-09-03 11:48:21 -0500

    Style landing page

--- in2it_site/pages/index.vue
+++ in2it_site/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
 	<div>
+		<div class="landing-page-content">
+			<h1 class="landing-page-title">Let's get into it!</h1>
+			<p class="landing-page-text">
+				A survey of our fellow Chicagoans found that there is a sizeable group that wants to be civically engaged, but faces obstacles to doing so. We want to make it easier for people to get involved in their communities.
+			</p>
+			<p class="landing-page-text">
+				Check out our list of community events so you can start making a difference!
+			</p>
+		</div>
 		<filters @filterApplied="updateEvents()"></filters>
 	</div>
 </template>
