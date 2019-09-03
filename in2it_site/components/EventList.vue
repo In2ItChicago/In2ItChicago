9669e7f512121e4c42ad723e310b47399ceea092
Wes <westhorburn@gmail.com>
2019-09-03 15:46:30 -0500

    Add rudimentary embedded map to events page

--- in2it_site/components/EventList.vue
+++ in2it_site/components/EventList.vue
@@ -1,12 +1,15 @@
 <template>
-	<div>
-		<div v-if="eventsAvailable">
+	<div class="content-row">
+		<div class="events-container" v-if="eventsAvailable">
 			<div v-for="event in events">
 				<event-listing :event="event"></event-listing>
 			</div>
 		</div>
 		<div v-else class="no-event-message">
 			<span>No events available, please adjust your search filter.</span>
+		</div>
+		<div class="map-container">
+			<iframe class="events-map" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDKuKo2WRNv5IhKm_At8wGfD4T142laung&q=222 W Merchandise Mart Plaza #470, Chicago, IL 60654" allowfullscreen></iframe>
 		</div>
 	</div>
 </template>
