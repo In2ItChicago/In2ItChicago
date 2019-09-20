91f6947a488b38c5df0f1e4a40a91553df1fe7f8
Wes <wjthorburn@live.com.au>
2019-09-20 11:01:57 -0500

    Update About Us text

--- in2it_site/pages/about-us.vue
+++ in2it_site/pages/about-us.vue
@@ -3,7 +3,17 @@
         <div class="content-col">
             <h1>About In2It</h1>
 
-            <p>The In2It team is an interdisciplinary group of tech-oriented Chicagoans who think that civic involvement matters. A survey of our fellow Chicagoans found that there is a sizeable group that wants to be civically engaged, but faces obstacles to doing so. We want to make it easier for people to get involved in their communities.</p>
+            <p>We are a group of Chicagoans that believe civic engagement matters, and that our city needs more people getting involved and making a difference in their communities.</p>
+
+            <p>This site started with a desire to get people more proactive about getting involved in their communities. Initially, the project was intended to become a civic health tracker to help people get motivated and track their projects. But after conducting a survey in preparation for the project, we found a much bigger issue. Many people already had the desire to be more involved, but they ran into difficulties finding the right opportunities for them.</p>
+
+            <p>There was a strong desire to find a more convenient place to search opportunities and target specific criteria that was important to them. They needed something quick, intuitive, and easy. It became clear that there were many common needs that were not being met by existing sites, creating unintentional barriers that nobody had a solution for.</p>
+
+            <p>After doing some research, we found that there was indeed a distinct need here that had to be filled. While there were many options out there, they were scattered across about a hundred different sites. Most of these sites were difficult to navigate and had very limited capabilities, and a limited set of opportunities.</p>
+
+            <p>We found our mission and set out to bring it to life, fittingly, through a weekly civic tech event called ChiHackNight. Over the course of this project, we’ve been delighted at the number of people interested in contributing and learning more about it. This website is a testament to what we’re all about, and what civic engagement really is. This would not be possible without all the people involved, working together to create something that will help their communities. And as we continue building and maintaining this website, it remains 100% volunteer run. We're passionate about our city, and making a difference for the people who live in it. Our hope is that this site will help people like you ignite or keep that passion as well.</p>
+
+            <p>Get inspired, get involved, get into it.</p>
 
             <p>This website is a work in progress, if you would like to get involved, we meet weekly at ChiHackNight. You can learn more about ChiHackNight and register for the next event at <a href="https://chihacknight.org/" target="_blank">chihacknight.org</a></p>
 
