e6807890499c6f377cc578bc620fbdd1d7f1c58d
Wes <wjthorburn@live.com.au>
2019-10-07 11:02:02 -0500

    Move footer links into navbar

--- in2it_site/components/PageFooter.vue
+++ in2it_site/components/PageFooter.vue
@@ -1,9 +1,7 @@
 <template>
 	<footer class="footer">
         <div class="container-fluid d-flex row justify-content-center">
-            <a class="footer-link" href="/">Home</a>
-            <a class="footer-link" href="/about-us">About Us</a>
-            <a class="footer-link" href="/join-us">Join Us</a>
+            
         </div>
     </footer>
 </template>
--- in2it_site/components/PageHeader.vue
+++ in2it_site/components/PageHeader.vue
@@ -5,8 +5,11 @@
                 <a class="in2it-brand" href="/">
                     <img class="navbar-logo" src="/img/in2it-logo.png">
                 </a>
-
-                <a class="nav-link" href="/events">Map</a>
+                <div class="in2it-nav-link-group">
+                    <a class="in2it-nav-link" href="/events">Map</a>
+                    <a class="in2it-nav-link" href="/about-us">About Us</a>
+                    <a class="in2it-nav-link" href="/join-us">Join Us</a>
+                </div>
             </div>
         </div>
     </nav>
