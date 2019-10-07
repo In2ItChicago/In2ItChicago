16ad22491edb12932462fbfe4fd80cb750ee56eb
Wes <wjthorburn@live.com.au>
2019-10-07 11:49:14 -0500

    Add ChiHackNight logo to the footer

--- in2it_site/components/PageFooter.vue
+++ in2it_site/components/PageFooter.vue
@@ -1,7 +1,9 @@
 <template>
 	<footer class="footer">
-        <div class="container-fluid d-flex row justify-content-center">
-            
+        <div class="container-fluid d-flex row justify-content-end">
+            <a href="https://chihacknight.org/" target="_blank">
+                <img src="/img/chihacknight-logo.png" class="chihacknight-logo" />
+            </a>
         </div>
     </footer>
 </template>
