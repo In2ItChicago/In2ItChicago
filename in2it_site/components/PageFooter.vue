58f53a88a3d11dfa96b1c09f1a0de82cf977e7bd
Wes <wjthorburn@live.com.au>
2019-10-21 13:20:14 -0500

    Color footer and change logo to light

--- in2it_site/components/PageFooter.vue
+++ in2it_site/components/PageFooter.vue
@@ -1,9 +1,11 @@
 <template>
 	<footer class="footer">
-        <div class="container-fluid d-flex row justify-content-end">
-            <a href="https://chihacknight.org/" target="_blank">
-                <img src="/img/chihacknight-logo.png" class="chihacknight-logo" />
-            </a>
+        <div class="container">
+            <div class="container-fluid d-flex row">
+                <a href="https://chihacknight.org/" target="_blank">
+                    <img src="/img/chihacknight-logo-light.png" class="chihacknight-logo" />
+                </a>
+            </div>
         </div>
     </footer>
 </template>
