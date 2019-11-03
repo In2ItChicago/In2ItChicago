c90e9fedd42898e340184fafdd5a9cd0b3ee2f71
Austin Schey <aschey13@gmail.com>
2019-11-02 22:41:34 -0500

    fix postgres start query

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$HOSTINGENV" = "DEV" ]
 then
-    until psql postgresql://postgres:postgres@postgres:5432/events -c "select 1" > /dev/null 2>&1; do
+    until psql postgresql://postgres:postgres@postgres:5432/postgres -c "select 1" > /dev/null 2>&1; do
         echo "Waiting for postgres server..."
         sleep 1
     done
