8026e87d101175905da3991d47082eb835184f35
Austin Schey <aschey13@gmail.com>
2019-11-21 00:02:01 -0600

    added text indexes

--- db/events/tables/events.Event.sql
+++ db/events/tables/events.Event.sql
@@ -19,5 +19,19 @@ WITH (
 )
 TABLESPACE pg_default;
 
+CREATE INDEX organization_search ON events.event USING GIN (to_tsvector('english', organization));
+CREATE INDEX event_title_search ON events.event USING GIN (to_tsvector('english', title));
+CREATE INDEX event_description_search ON events.event USING GIN (to_tsvector('english', description));
+
+
 ALTER TABLE events.Event
     OWNER to postgres;
+-- http://rachbelaid.com/postgres-full-text-search-is-good-enough/
+-- SELECT title, description, ts_rank_cd(to_tsvector(description), query) AS descscore, null as titlescore
+-- FROM  events.event, to_tsquery('year') query
+-- WHERE description @@ query
+-- UNION
+-- SELECT title, description, null as descscore, ts_rank_cd(to_tsvector(title), query) AS descscore
+-- FROM  events.event, to_tsquery('year') query
+-- WHERE title @@ query
+-- order by titlescore desc, descscore desc
