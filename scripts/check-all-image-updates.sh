d04a7456e05407a4d91e1aee32e0d16863693b1e
Austin Schey <aschey13@gmail.com>
2020-03-06 21:03:41 -0600

    bump images

--- scripts/check-all-image-updates.sh
+++ scripts/check-all-image-updates.sh
@@ -6,12 +6,12 @@ scriptpath=$(dirname $0)/check_image_updates.py
 echo
 echo "============================="
 echo "Checking for image updates..."
-$scriptpath library/python 3.8 alpine
+$scriptpath library/python 3.8.2 alpine
 $scriptpath library/postgres 11.5 alpine
-$scriptpath dpage/pgadmin4 4.15
-$scriptpath lucaslorentz/caddy-docker-proxy 0.3.3 alpine
-$scriptpath library/node 13.2 alpine
-$scriptpath portainer/portainer 1.22.2
-$scriptpath scrapinghub/splash 3.4
+$scriptpath dpage/pgadmin4 4.19
+$scriptpath lucaslorentz/caddy-docker-proxy 0.3.6 alpine
+$scriptpath library/node 13.10.1 alpine
+$scriptpath portainer/portainer 1.23.1
+$scriptpath scrapinghub/splash 3.4.1
 echo "=============================="
 echo
