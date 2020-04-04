fd9b7bc0f834c91d37f0fa5f202d53f736ad2aeb
Wes <wjthorburn@live.com.au>
2020-04-04 17:47:31 -0500

    Add 'A ChiHackNight Project' to footer

--- in2it_site/components/PageFooter.vue
+++ in2it_site/components/PageFooter.vue
@@ -1,15 +1,38 @@
 <template>
 	<footer class="footer">
         <div class="container">
-            <div class="container-fluid d-flex row">
-                <a href="https://chihacknight.org/" target="_blank">
-                    <img src="/img/chihacknight-logo-light.png" class="chihacknight-logo" />
-                </a>
+            <div class="container-fluid footer-container justify-content-end">
+                <div>
+                    <a href="https://chihacknight.org/" target="_blank">
+                        <img src="/img/chihacknight-logo-light.png" class="chihacknight-logo" />
+                    </a>
+                    <p class="hacknight-project-label">A ChiHackNight project</p>
+                </div>
             </div>
         </div>
     </footer>
 </template>
 
-<script>
-	export default{};
-</script>
+<style scoped>
+    .chihacknight-logo{
+        height:65px;
+        width:auto;
+    }
+
+    .footer{
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        background-color:#173450;
+    }
+
+    .footer-container{
+        display:flex;
+    }
+
+    .hacknight-project-label{
+        color: #fff;
+        font-size: 18px;
+        font-family: 'Nunito', 'regular';
+    }
+</style>
