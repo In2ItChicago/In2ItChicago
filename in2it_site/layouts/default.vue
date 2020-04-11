ebff3f5aa354dcb8d1734f33e9e88f9f2c7c13df
Wes <wjthorburn@live.com.au>
2020-04-11 12:26:06 -0500

    Add covid alert to default page layout

--- in2it_site/layouts/default.vue
+++ in2it_site/layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="site">
+      <v-alert prominent class="covid-alert">
+        <v-row align="center">
+          <v-col class="d-flex flex-row">
+            <v-icon>
+              mdi-alert-circle
+            </v-icon>
+            <div class="covid-alert-text">
+              To help in the fight against COVID-19, we recommend avoiding in-person civic activities. Instead, consider the <a href="/get-involved-from-home">following projects</a> where you can get involved from home.
+            </div>
+            <v-btn class="d-flex get-involved-btn" href="/get-involved-from-home">
+              Get Involved From Home
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
       <header>
 			  <page-header></page-header>
 		  </header>
@@ -23,3 +38,28 @@
       }
     };
 </script>
+
+<style scoped>
+  .covid-alert{
+    color: #ff5252 !important;
+    background-color: #ffe9eb;
+  }
+
+  .covid-alert-text{
+    font-size: 20px;
+    margin-left:10px;
+  }
+
+  .covid-alert i {
+    color: red;
+  }
+
+  .get-involved-btn{
+    display:flex;
+    margin-left:auto;
+  }
+
+  .get-involved-btn:hover{
+    text-decoration: none;
+  }
+</style>
