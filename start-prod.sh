1f00f75bef6a98b53332085c5ac7040e4b72962e
Austin Schey <aschey13@gmail.com>
2020-04-18 18:47:06 -0500

    env stuff

--- start-prod.sh
+++ start-prod.sh
@@ -8,5 +8,5 @@ docker container prune -f
 sleep 5
 
 docker build -f db/Dockerfile -t db_deploy db
-source .env-prod && ./scripts/stack-deploy.sh prod
+env $(cat .env-prod | xargs) ./scripts/stack-deploy.sh prod
 ./db/run.sh PROD
