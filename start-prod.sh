7f62bf81a7369f2b9ac3722beea53c62bfd61240
aschey <aschey13@gmail.com>
2020-12-31 17:03:38 -0500

    tls config

--- start-prod.sh
+++ start-prod.sh
@@ -11,4 +11,4 @@ docker build -f db/Dockerfile -t db_deploy db
 docker build -f create_schedules/Dockerfile -t create_schedules create_schedules
 docker build -f caddy/Dockerfile -t caddy_config caddy
 env $(cat .env-prod | xargs) ./scripts/stack-deploy.sh prod
-./db/run.sh PROD
+#./db/run.sh PROD
