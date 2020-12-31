57f8bde37b03e05a42ce99eca56d5f98b9d7bb19
aschey <aschey13@gmail.com>
2020-12-31 14:41:02 -0500

    update ssh scripts

--- deploy/connect-ports.sh
+++ deploy/connect-ports.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 3000:localhost:3000 ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 5000:localhost:5000 ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 8888:localhost:8888 ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 6800:localhost:6800 ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 5432:localhost:5432 ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
-ssh -nNT -i $(dirname $0)/in2it-key.pem -L 2376:/var/run/docker.sock ec2-user@ec2-3-16-31-88.us-east-2.compute.amazonaws.com &
+ssh -nNT -i ~/.ssh/id_rsa -L 3000:127.0.0.1:3000 in2it@104.207.139.250 &
+ssh -nNT -i ~/.ssh/id_rsa -L 5000:127.0.0.1:5000 in2it@104.207.139.250 &
+ssh -nNT -i ~/.ssh/id_rsa -L 8888:127.0.0.1:8888 in2it@104.207.139.250 &
+ssh -nNT -i ~/.ssh/id_rsa -L 5432:/var/run/postgresql/.s.PGSQL.5432 in2it@104.207.139.250 &
+ssh -nNT -i ~/.ssh/id_rsa -L 2376:/var/run/docker.sock in2it@104.207.139.250 &
