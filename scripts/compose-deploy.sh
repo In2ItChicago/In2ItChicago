12c8bb30d58644f166cfb2fefd2381b41cc7dade
Austin Schey <aschey13@gmail.com>
2021-07-13 19:20:43 -0500

    fix some build issues

--- scripts/compose-deploy.sh
+++ scripts/compose-deploy.sh
@@ -5,7 +5,7 @@ PARAMS=""
 EVENT_PROCESSOR_DEBUG=0
 SCHEDULER_DEBUG=0
 VERBOSE_OUTPUT=0
-EXCLUDE=''
+EXCLUDE='ndscheduler\|create_schedules'
 RUN_SCHEDULER=0
 ENV="dev"
 SPIDER_NAME=""
@@ -13,72 +13,72 @@ ENABLE_RESPONSE_CACHE=1
 BYPASS_AUTH=1
 
 # use auth if auth keys exist
-if test -f "$(dirname $0)/../.env"; then 
-  BYPASS_AUTH=0
+if test -f "$(dirname $0)/../.env"; then
+    BYPASS_AUTH=0
 fi
 
 while (( "$#" )); do
-  case "$1" in
-    -c|--scheduler-debug)
-      SCHEDULER_DEBUG=1
-      shift
-      ;;
-    -d|--processor-debug)
-      EVENT_PROCESSOR_DEBUG=1
-      shift
-      ;;
-    -e|--env)
-      ENV=$2
-      shift 2
-      ;;
-    -v|--verbose-output)
-      VERBOSE_OUTPUT=1
-      shift
-      ;;
-    -s|--scheduler)
-      EXCLUDE=""
-      RUN_SCHEDULER=1
-      shift
-      ;;
-    -n|--spider-name)
-      SPIDER_NAME=$2
-      shift 2
-      ;;
-    -r|--disable-response-cache)
-      ENABLE_RESPONSE_CACHE=0
-      shift
-      ;;
-    --) # end argument parsing
-      shift
-      break
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Error: Unsupported flag $1" >&2
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS="$PARAMS $1"
-      shift
-      ;;
-  esac
+    case "$1" in
+        -c|--scheduler-debug)
+            SCHEDULER_DEBUG=1
+            shift
+        ;;
+        -d|--processor-debug)
+            EVENT_PROCESSOR_DEBUG=1
+            shift
+        ;;
+        -e|--env)
+            ENV=$2
+            shift 2
+        ;;
+        -v|--verbose-output)
+            VERBOSE_OUTPUT=1
+            shift
+        ;;
+        -s|--scheduler)
+            EXCLUDE=""
+            RUN_SCHEDULER=1
+            shift
+        ;;
+        -n|--spider-name)
+            SPIDER_NAME=$2
+            shift 2
+        ;;
+        -r|--disable-response-cache)
+            ENABLE_RESPONSE_CACHE=0
+            shift
+        ;;
+        --) # end argument parsing
+            shift
+            break
+        ;;
+        -*|--*=) # unsupported flags
+            echo "Error: Unsupported flag $1" >&2
+            exit 1
+        ;;
+        *) # preserve positional arguments
+            PARAMS="$PARAMS $1"
+            shift
+        ;;
+    esac
 done
 # set positional arguments in their proper place
 eval set -- "$PARAMS"
 
-if [ ! -z "$EXCLUDE" ] && [ -z "$PARAMS" ] 
+if [ ! -z "$EXCLUDE" ] && [ -z "$PARAMS" ]
 then
     case "$(uname)" in
-    CYGWIN*|MINGW*|MSYS*)
-        # Carriage returns ruin everything
-        SERVICES=$(docker-compose config --services | grep -v -e $EXCLUDE | tr '\r\n' ' ')
+        CYGWIN*|MINGW*|MSYS*)
+            # Carriage returns ruin everything
+            SERVICES=$(docker-compose config --services | grep -v -e $EXCLUDE | tr '\r\n' ' ')
         ;;
-    *)
-        SERVICES=$(docker-compose config --services | grep -v -e $EXCLUDE)
+        *)
+            SERVICES=$(docker-compose config --services | grep -v -e $EXCLUDE)
         ;;
     esac
-  
+    
 else
-  SERVICES=$PARAMS
+    SERVICES=$PARAMS
 fi
 
 SCHEDULER_DEBUG=$SCHEDULER_DEBUG \
