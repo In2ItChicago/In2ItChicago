0617f0bcf58717802053a622b46dff924e18c05a
Wes Thorburn <westhorburn@gmail.com>
2021-08-23 20:30:58 -0500

    Add privacy policy and terms & conditions pages

---
+++ in2it_site/pages/privacy-policy.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="content-col col-md-8 offset-md-2 col-sm-12">
+            <h1>Privacy Policy</h1>
+            <p>This privacy policy ("policy") will help you understand how IN2ITChicago ("us", "we", "our") uses and protects the data you provide to us when you visit and use in2itchicago.com ("website", "service"). We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise that you frequently visit this page.</p>
+
+            <h2>What User Data We Collect</h2>
+            <p>When you visit the website, we may collect the following data: </p>
+            <ul>
+                <li>IP address. </li>
+                <li>Personal information including but not limited to your name and email address if you have explicitly provided it to us. </li>
+                <li>Other information such as interests and preferences. </li>
+                <li>Data profile regarding your online behavior on our website. </li>
+            </ul>
+
+            <h2>Why We Collect User Data </h2>
+            <p>We are collecting your data for several reasons: </p>
+            <ul>
+                <li>To improve our services and products. </li>
+                <li>To better understand your needs. </li>
+                <li>To send you promotional emails containing the information we think you will find interesting.</li>
+                <li>To contact you to fill out surveys and participate in other types of market research. </li>
+                <li>To customize our website according to your online behavior and personal preferences. </li>
+            </ul>
+
+            <p>Every promotional email will contain an ‘Unsubscribe,’ link. Clicking this link will immediately unsubscribe you from all further promotional emails. You have the ability to unsubscribe from promotional emails any time. </p>
+
+            <h2>Safeguarding and Securing the Data </h2>
+            <p>IN2ITChicago is committed to securing your data and keeping it confidential. We have done all in our power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online. </p>
+
+            <h2>Our Cookie Policy </h2>
+            <p>Once you agree to allow our website to use cookies, you also agree to use the data it collects regarding your online behavior (analyze website activity, web pages you spend the most time on, and websites you visit). </p>
+
+            <p>Some of the data we collect from cookies may contribute to further development of the site. After we use the data for statistical analysis, the data is completely removed from our systems. </p>
+
+            <p>Please note that cookies don't allow us to gain control of your computer in any way. They are strictly used to monitor which pages you find useful and which you do not so that we can provide a better experience for you.</p>
+
+            <p>If you want to disable cookies, you can do it by accessing the settings of your internet browser. (Provide links for cookie settings for major internet browsers, Google Chrome, Firefox, Safari, Microsoft Edge, Opera). </p>
+
+            <h2>Links to Other Websites </h2>
+            <p>Our website contains links that lead to other websites. If you click on these links IN2ITChicago is not held responsible for your data and privacy protection once you leave our site. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website. </p>
+
+            <h2>Restricting the Collection of your Personal Data </h2>
+            <p>At any point, if you wish to restrict the collection of your personal data or permanently remove already collected personal data from the website, please contact us via email. You will be required to provide basic identifying information such as your name and email in order for us to locate and remove your personal data.</p>
+
+            <p>IN2ITChicago will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us. Your personal information will be used when we need to send you promotional materials if you agree to this privacy policy. </p>
+        </div>
+    </div>
+</template>
